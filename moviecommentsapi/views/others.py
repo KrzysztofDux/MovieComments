81f97cd62adc1dc53a85ccd7b59305422e3f1c5b
Krzysztof Kulak <krzysztofpiotrdux@gmail.com>
2019-09-08 12:49:54 +0200

    validated top view agains wrong date formats

--- moviecommentsapi/views/others.py
+++ moviecommentsapi/views/others.py
@@ -9,14 +9,17 @@ from ..models import Movie, Comment
 
 @api_view(['GET'])
 def top(request):
-    if all(param in request.query_params for param in ("from", "to")):
+    try:
         date_from, date_to = request.query_params.get("from"), request.query_params.get("to")
         date_from, date_to = datetime.strptime(date_from, '%d %b %Y'), datetime.strptime(date_to,
                                                                                          '%d %b %Y')
         include_all = include_all_param_to_bool(request.query_params)
         return JsonResponse(get_ranking(date_from, date_to, include_all), safe=False)
-    else:
+    except (KeyError, TypeError):
         return JsonResponse({"message": "no date range provided"},
+                            status=status.HTTP_400_BAD_REQUEST)
+    except ValueError:
+        return JsonResponse({"message": "wrong date format provided"},
                             status=status.HTTP_400_BAD_REQUEST)
 
 
